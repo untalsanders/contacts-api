54621d386ca63a4e7402412b311a0e0c266cf370
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-06-07 05:13:07 -0300

    refactor: stpe-2 move global exception handler to the package rest
    The @ControllerAdvice of Spring is responsable for mapping excepciones
    to HTTP responses. This is a concern of the rest (how the system
    communicates externally), not of the domain or infrastructure.
    
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/main/java/com/untalsanders/contacts/rest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/untalsanders/contacts/rest/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.untalsanders.contacts.shared.exception;
+package com.untalsanders.contacts.rest.exception;
 
 import com.untalsanders.contacts.shared.domain.ErrorMessage;
 import com.untalsanders.contacts.shared.domain.ErrorResponse;
--- src/main/java/com/untalsanders/contacts/rest/exception/SuchElementAlreadyExistsException.java
+++ src/main/java/com/untalsanders/contacts/rest/exception/SuchElementAlreadyExistsException.java
@@ -1,4 +1,4 @@
-package com.untalsanders.contacts.shared.exception;
+package com.untalsanders.contacts.rest.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
